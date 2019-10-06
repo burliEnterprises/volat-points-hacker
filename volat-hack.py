e5bafbaaf6b0720371bd2b425e57c215c419263e
Matteo Kofler <matteokofler1@gmail.com>
2019-10-06 15:12:00 +0200

    didnt work

--- volat-hack.py
+++ volat-hack.py
@@ -101,10 +101,10 @@ def hackThemPoints():
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--disable-gpu')
     chrome_options.add_argument('--no-sandbox')
-    #chrome_options.binary_location = "/app/.apt/usr/bin/google-chrome-stable"
-    chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-    #browser = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
-    browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+    chrome_options.binary_location = "/app/.apt/usr/bin/google-chrome-stable"
+    #chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
+    browser = webdriver.Chrome(executable_path='/app/.chromedriver/bin/chromedriver', chrome_options=chrome_options)
+    #browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
     # OG code:
     # https://www.guru99.com/selenium-python.html
     # https://selenium-python.readthedocs.io/locating-elements.html
